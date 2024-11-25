0a42976938fd5f4dd469d3ab9f3a28dbde22e490
anudammm <125276295+anudammm@users.noreply.github.com>
2024-11-25 19:42:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import os
 from data_preparation import prepare_data
 from train_model import train_the_model
+from train_canny_model import train_canny
 
 if __name__ == '__main__':
     """
         If you want to train a model on a specific dataset,
         you need to change the class mapping in train_model.py
     """
-    if os.path.exists("data.csv"):
-        train_the_model()
+    canny = True
+    if canny:
+        num_epochs = 10
+        train_canny_model(10)
     else:
-        prepare_data()
-        train_the_model()
+        if os.path.exists("data.csv"):
+            train_the_model()
+        else:
+            prepare_data()
+            train_the_model()
